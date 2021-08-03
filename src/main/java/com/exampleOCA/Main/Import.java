8d448a8d7d4d30723cb8cf5c04cb2ea57e94256b
Loredana <lore.puiu@yahoo.com>
2021-08-03 22:32:26 +0300

    Initial commit

---
+++ src/main/java/com/exampleOCA/Main/Import.java
@@ -0,0 +1,10 @@
+package com.exampleOCA.Main;
+
+import java.util.Random;
+
+public class Import {
+    public static  void  main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10)); // print a number between 0 and 9
+    }
+}
